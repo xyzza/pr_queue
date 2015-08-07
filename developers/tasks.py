333967c5d1b70bf072699bf17134df8f59e39e6f
Тимур Ахмадиев <akhmadiev@bars-open.ru>
2015-08-07 08:22:03 +0300

    some files moved. celery added

---
+++ developers/tasks.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+from celery.task.schedules import crontab
+from celery.decorators import periodic_task
+from celery.utils.log import get_task_logger
+
+from prqueue.queue import send_all_assignments
+
+logger = get_task_logger(__name__)
+
+@periodic_task(
+    run_every=(crontab(minute='*/2')),
+    name='test_apply',
+    ignore_result=True,
+)
+def test_apply():
+    logger.info("Sending assignments")
+    send_all_assignments()
