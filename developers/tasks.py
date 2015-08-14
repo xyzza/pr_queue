aeaae7ede0a1dbc92597d2699c5009ec6d36ffaf
Тимур Ахмадиев <akhmadiev@bars-open.ru>
2015-08-14 18:41:00 +0300

    work on celery schedule

--- developers/tasks.py
+++ developers/tasks.py
@@ -1,7 +1,11 @@
 # coding: utf-8
+from collections import defaultdict
+
 from celery.utils.log import get_task_logger
 from celery.task.schedules import crontab
 from celery.decorators import periodic_task
+
+from django.conf import settings
 
 from developers.queue import send_all_assignments
 
@@ -10,11 +14,15 @@ logger = get_task_logger(__name__)
 
 
 # TODO: make this time configurable
+_schedule = defaultdict(hour=5, minute=42, day_of_week=[1, 2, 3, 4, 5])
+if settings.DEBUG:
+    _schedule = defaultdict(minute='*/2')
+
 @periodic_task(
-    run_every=(crontab(minute='*/2')),
-    name='test_apply',
+    run_every=(crontab(**_schedule)),
+    name='apply_assignments',
     ignore_result=True,
 )
-def test_apply():
-    logger.info("Sending assignments")
+def apply_assignments():
+    logger.info("Sending all assignments")
     send_all_assignments()
