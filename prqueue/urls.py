5f3b04325501abdebb96abdc60d5c73b5c10d3da
Akhmadiev Timur <yaxyzza@gmail.com>
2015-09-30 21:55:30 +0300

    dj rest fixed

--- prqueue/urls.py
+++ prqueue/urls.py
@@ -1,10 +1,11 @@
 # coding: utf-8
 from django.conf.urls import include, url
 from django.contrib import admin
-from .rest import router
+from .rest import rest_urls
+
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
+
+urlpatterns += rest_urls
