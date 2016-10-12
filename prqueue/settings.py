0d01cde580903331cbecf35401aad5d26cdad5be
Tim <yaxyzza@gmail.com>
2016-10-12 22:26:42 +0300

    rebase merge

--- prqueue/settings.py
+++ prqueue/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+g%2gck725lg6ilah5h)obimt3*_x9r7q#0&(&9ak7s&tnbg)s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 
 # Application definition
