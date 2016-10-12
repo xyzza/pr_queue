33ebde55839873aa0f78363acd3d06baf60941cd
Akhmadiev Timur <xyzza@MacBook-Pro-Akhmadiev.local>
2016-10-12 22:20:12 +0300

    send mail added. assignment message reformated

--- developers/tasks.py
+++ developers/tasks.py
@@ -1,13 +1,15 @@
 # coding: utf-8
 from celery.utils.log import get_task_logger
-
 from celery.task.schedules import crontab
 from celery.decorators import periodic_task
+
 from developers.queue import send_all_assignments
 
 
 logger = get_task_logger(__name__)
 
+
+# TODO: make this time configurable
 @periodic_task(
     run_every=(crontab(minute='*/2')),
     name='test_apply',
